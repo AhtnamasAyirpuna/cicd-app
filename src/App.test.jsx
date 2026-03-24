3609ee67cf8cdc70ca9482fb7765dba02987da22
Samantha Anupriya <samanthaanupriya98@gmail.com>
2026-03-24 21:51:35 +0800

    initial commit

---
+++ src/App.test.jsx
@@ -0,0 +1,18 @@
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+
+test("App form submission", async () => {
+    render(<App />);
+
+    //get the email input and submit button
+    const emailInput = screen.getByTestId("emailInput");
+    const submitButton = screen.getByTestId("submitButton");
+
+    //Type a valid email into the input
+    await userEvent.type(emailInput, 'sigmaschool@gmail.com');
+
+    //check if button is disabled
+    expect(submitButton.disabled).toBe(false);
+})
